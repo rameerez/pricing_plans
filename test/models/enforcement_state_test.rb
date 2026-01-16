99b8803bc3dd13e9a583ab520860ac62c12c5d71
Stuart Yamartino <stu@stuyam.com>
2026-01-16 11:11:19 -0500

    test with different limit key

--- test/models/enforcement_state_test.rb
+++ test/models/enforcement_state_test.rb
@@ -39,6 +39,13 @@ class EnforcementStateTest < ActiveSupport::TestCase
 
     refute duplicate.valid?
     assert duplicate.errors[:limit_key].any?
+
+    differrent_limit_key = PricingPlans::EnforcementState.new(
+      plan_owner: @org,
+      limit_key: "members"
+    )
+
+    assert differrent_limit_key.valid?
   end
 
   def test_exceeded_scope_and_method
